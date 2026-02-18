decc6e0c4124a96df8bb5f9f31ff4a69b80e3019
Tag <tagtribble@gmail.com>
2026-02-17 21:37:58 -0500

    Updated Comments

--- field_generator.py
+++ field_generator.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 #function to get E, im using a list temporily 
-# MUST DEAL WITH pos LATER
+# MUST DEAL WITH PASSING pos LATER
 def get_e(q, pos, x, y):
     dx, dy = x - pos[0], y - pos[1]
     r = np.sqrt(dx**2 + dy**2)
@@ -21,16 +21,18 @@ x = np.linspace(-50, 50, 25)
 y = np.linspace(-50, 50, 25)
 X, Y = np.meshgrid(x, y)
 
+#Defining charges
 #(q, (x, y)) q is 1 for positive and -1 for negative
 charge = (-1, (-3, 0)) #Positive charge at x = -3
 q, pos = charge
 
+# Getting the points from a charge, will need to be able to average points later
 U, V, ex, ey = get_e(q, pos, X, Y)
-M = np.hypot(ex, ey)
+M = np.hypot(ex, ey) #Creates color map, need to figure out how to normalize it
 
-#I have ran into a problem, I need to figure out how to 
-# get U and, problem is, I have no clue what they are and how to get 
-# them from X and Y -NOTEBOOK-22
+#plots the map
 plt.quiver(X, Y, U, V, M, pivot='tip')
 plt.show()
+
+#Printing M to help figure out how to normalize
 print(M)

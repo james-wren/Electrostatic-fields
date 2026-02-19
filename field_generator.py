a9197b6650201220267c25f67ca51d81f1627f26
Tag <tagtribble@gmail.com>
2026-02-18 19:04:53 -0500

    Got multiple charges working, need to figure out normalization for arrows.

--- field_generator.py
+++ field_generator.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-
 #function to get E, im using a list temporily 
 # MUST DEAL WITH PASSING pos LATER
 def get_e(q, pos, x, y):
@@ -10,11 +9,29 @@ def get_e(q, pos, x, y):
     r = np.sqrt(dx**2 + dy**2)
 
     U = q * dx / r
-    V = q * dy / r
+    V = q * dy / r 
     ex = q * dx / r**3
     ey = q * dy / r**3
 
     return U, V, ex, ey
+
+def find_points(charges):
+    U, V, M = None, None, None
+    first = True
+    for q, pos in charges:
+        u, v, ex, ey = get_e(q, pos, X, Y)
+        m = np.hypot(ex, ey)
+        m = m.flatten()
+        if not first:
+            U = (U + u)
+            V = (V + v)
+            M = (M + m)
+        else:
+            U = u
+            V = v
+            M = m
+            first = False
+    return U, V, M
 
 #I now need to set up my graph with mpl and np
 x = np.linspace(-50, 50, 25)
@@ -23,16 +40,14 @@ X, Y = np.meshgrid(x, y)
 
 #Defining charges
 #(q, (x, y)) q is 1 for positive and -1 for negative
-charge = (-1, (-3, 0)) #Positive charge at x = -3
-q, pos = charge
+charges = [(-1, (-3, 0)), (1, (3, 0))] #Positive charge at x = -3
 
 # Getting the points from a charge, will need to be able to average points later
-U, V, ex, ey = get_e(q, pos, X, Y)
-M = np.hypot(ex, ey) #Creates color map, need to figure out how to normalize it
+#Creates color map, need to figure out how to normalize it
+#Turns the 2d array into a 1d array, better for later nomalization
+U, V, M = find_points(charges)
+norm = mpl.colors.LogNorm(vmin=1e-4, vmax=1.0)
 
 #plots the map
-plt.quiver(X, Y, U, V, M, pivot='tip')
+plt.quiver(X, Y, U, V, M, norm=norm, cmap='plasma', pivot='tip')
 plt.show()
-
-#Printing M to help figure out how to normalize
-print(M)
